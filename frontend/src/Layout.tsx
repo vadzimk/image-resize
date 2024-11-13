e41fa4dcfe29c6bc0b090755074cba14246a922c
Vadzim <vadzimk@hotmail.com>
2024-11-13 16:56:08 +0300

    frontend: configure and layout
    - add dependencies:
      - react-router-dom
      - daisyui
      - tailwindcss
      - swc/jest
    - setup Layout to show all in the middle of page
    - add ErrorPage
    - configure vite proxy to backend

---
+++ frontend/src/Layout.tsx
@@ -0,0 +1,11 @@
+import {Outlet} from 'react-router-dom';
+
+export default function Layout(){
+    return(
+        <div className="screen-height flex flex-row justify-center">
+            <div className="flex flex-col justify-center">
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+      '/socket.io' : {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        ws: true, // Enable WebSocket proxying
+      }
+    }
+  },
 })
